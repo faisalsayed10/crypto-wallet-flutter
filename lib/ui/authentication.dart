4a852fa47f637bd4337ed8e270c303e5157cd464
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-02 15:04:33 +0530

    Authentication page

---
+++ lib/ui/authentication.dart
@@ -0,0 +1,85 @@
+import 'package:crypto_wallet_project/net/flutterfire.dart';
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: Colors.blueAccent),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextFormField(
+              controller: _emailField,
+              decoration: InputDecoration(
+                hintText: "something@email.com",
+                hintStyle: TextStyle(color: Colors.white),
+                labelText: "Email",
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: _passwordField,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "23horses!ntHeSt@ble",
+                hintStyle: TextStyle(color: Colors.white),
+                labelText: "Password",
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await register(_emailField.text, _passwordField.text);
+                  
+                  if (shouldNavigate) {
+
+                  }
+                },
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await signIn(_emailField.text, _passwordField.text);
+
+                  if (shouldNavigate) {}
+                },
+                child: Text("Login"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
